52b307e31c5578b62f9204e81024cb05fd2e5bd5
RÁDIO SUPER NOTICIA <gabrielfelipe8637@gmail.com>
2026-05-31 23:35:18 -0300

    Update programacao-data.js

--- programacao-data.js
+++ programacao-data.js
@@ -4,7 +4,7 @@ window.PROGRAMACAO = {
       { time: '00:00', title: 'Itatiaia é a Dona da Noite' },
       { time: '04:00', title: 'PARADÃO SERTANEJO' },
       { time: '06:00', title: 'Tiro de Meta' },
-      { time: '06:30', title: 'Jornal da CBN'
+      { time: '06:30', title: 'Jornal da CBN' },
       { time: '07:00', title: 'Jornal da Itatiaia' },
       { time: '09:00', title: 'Bom Dia Cidade' },
       { time: '10:00', title: 'Giro Na Copa' },
@@ -16,8 +16,8 @@ window.PROGRAMACAO = {
       { time: '17:00', title: 'Ponto Final CBN' },
       { time: '19:00', title: 'A VOZ DO BRASIL' },
       { time: '20:00', title: 'Quatro Em Campo/Jornada Esportiva' },
-      { time: '21:00', title: 'Futebol Globo CBN' },
-      { time: '22:00', title: 'Futebol Globo CBN' },
+      { time: '21:00', title: 'Conexão Super Brasil/Futebol Globo CBN' },
+      { time: '22:00', title: 'Noite Livre/Futebol Globo CBN' },
       { time: '23:00', title: 'Love Songs/Futebol Globo CBN' }
     ],
     'sab': [
