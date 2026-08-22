a47bd2c1ac7be2bd76579f2fd643761ee15cd86a
RÁDIO SUPER NOTICIA <gabrielfelipe8637@gmail.com>
2026-08-22 20:35:07 -0300

    Change match status and initialize goal counts
    Updated match status to 'AO VIVO' and set goals to 0.

--- script.js
+++ script.js
@@ -270,15 +270,15 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     {
       campeonato: "Brasileirão",
-      status: "A SEGUIR",
+      status: "AO VIVO",
       data: "22/08",
       hora: "20:30",
       time_casa: "Cru.",
       escudo_casa: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Cruzeiro_Esporte_Clube_%28logo%29.svg/250px-Cruzeiro_Esporte_Clube_%28logo%29.svg.png?utm_source=pt.wikipedia.org&utm_campaign=parser&utm_content=thumbnail",
-      gols_casa: null,
+      gols_casa: 0,
       time_fora: "Flamengo",
       escudo_fora: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Clube_de_Regatas_do_Flamengo_logo.svg/250px-Clube_de_Regatas_do_Flamengo_logo.svg.png?utm_source=pt.wikipedia.org&utm_campaign=parser&utm_content=thumbnail",
-      gols_fora: null
+      gols_fora: 0
     },
     {
       campeonato: "Brasileirão",
